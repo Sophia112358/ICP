e54b962f13361131490c4f9386c5ea8c51094e0d
Sophia112358 <sophia1123lyj@gmail.com>
2024-05-23 14:14:21 +0900

    Update with video

--- data/2ply.py
+++ data/2ply.py
@@ -1,29 +1,27 @@
 import open3d as o3d
 import numpy as np
 
-# 文件路径
 input_filename = "./44.txt"
 output_filename = "./44.ply"
 
-# 读取数据
+# load
 points = []
 colors = []
 with open(input_filename, 'r') as file:
     for line in file:
-        # 解析每行的XYZ坐标和RGB颜色
         parts = line.split()
-        x, y, z = map(float, parts[:3])  # 读取XYZ坐标
-        r, g, b = map(int, parts[3:])   # 读取RGB颜色
+        x, y, z = map(float, parts[:3])  # read coordinate
+        r, g, b = map(int, parts[3:])   # read RGB
         points.append([x, y, z])
-        # 将RGB颜色从[0, 255]规范化到[0, 1]范围
+        # normalize RGB
         colors.append([r / 255.0, g / 255.0, b / 255.0])
 
-# 创建Open3D点云对象
+# create point cloud
 pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector(np.array(points))
 pcd.colors = o3d.utility.Vector3dVector(np.array(colors))
 
-# 保存为PLY文件
+# save
 o3d.io.write_point_cloud(output_filename, pcd)
 
 print("Point cloud with colors saved to", output_filename)
